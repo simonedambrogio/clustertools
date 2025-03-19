43a5250ec4e258871dd20f53fef00e0f9221cd9b
simonedambrogio <simonedambrogio@live.it>
2025-03-19 14:56:34 +0000

    update export

--- clustertools/__init__.py
+++ clustertools/__init__.py
@@ -1,11 +1,8 @@
-from .local2cluster import local2cluster, login2ssh, send_file, send_folder
+from .local2cluster import local2cluster
 from .cluster2local import cluster2local
 __version__ = '0.1.0'
 
 __all__ = [
     'local2cluster',
-    'login2ssh',
-    'send_file',
-    'send_folder',
     'cluster2local'
 ] 
